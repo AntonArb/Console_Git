1f84421c19ba0d2c627cc7d285b2ea0fb3d9ba53
user <anton.borisov.m@gmail.ru>
2025-07-14 06:21:23 +0300

    pr1

---
+++ Console_Git/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Git
+{
+    internal class Class1
+    {
+        public NewClass()
+        {
+            Console.WriteLine("Команда разработчиков 1");
+        }
+
+    }
+}
